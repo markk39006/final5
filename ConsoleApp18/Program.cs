abc51d7f2fe7052fd778b8d8eb58b8134c263832
markk39006 <markk39006@gmail.com>
2021-06-13 17:00:31 +0700

    1

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[,] itemnametype = itemnametype = new string[n,2];
+            for (int i = 0; i <= n-1; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+                    itemnametype[i, j] = Console.ReadLine();
+                }
+            }
+            string mode = Console.ReadLine();
+            if (mode == "ShowAll")
+            {
+                for (int i = 0; i <= n - 1; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        Console.WriteLine(itemnametype[i, j]);
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("End");
+            }
+        }
+    }
+}
